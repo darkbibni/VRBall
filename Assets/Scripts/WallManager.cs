a1138f35df450625e5de0f96e6725a6d12a86482
unknown <e.escher@J001-0291.etudiant.rubika-edu.com.local>
2017-10-10 16:03:08 +0200

    premier commit => Script et object wall

---
+++ Assets/Scripts/WallManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class WallManager : MonoBehaviour {
+
+    public float delayWall = 60;
+    private float timer;
+    private int numWall;
+    public Transform[] walls;
+
+	// Use this for initialization
+	void Start () {
+        timer = 0;
+        numWall = 0;
+        StartCoroutine("TimerWall");
+	}
+
+    private IEnumerator TimerWall()
+    {
+        yield return new WaitForSeconds(delayWall);
+        if (numWall != 3)
+        {
+            walls[numWall].DOLocalMoveY(-3, 2);
+            numWall++;
+        }else if (numWall == 3)
+        {
+            Destroy(this.gameObject);
+        }
+        StartCoroutine("TimerWall");
+    }
+
+}
