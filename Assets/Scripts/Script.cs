87383bae0113f9f7da72ea20e20418f114c5a061
ETU\l.bertaux <h4ck3r59@hotmail.fr>
2017-10-10 14:13:38 +0200

    SteamVR and hierarchy

---
+++ Assets/Scripts/Script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+	public class Script : MonoBehaviour {
+
+		#region Inspector attributes
+		
+		#endregion
+		
+		#region Private attributes
+		
+		#endregion
+		
+		#region Unity methods
+		
+		// Use this for initialization
+		void Awake () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+		
+		#endregion
+	}
+}
