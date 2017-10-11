c0bcb215d2c481e6e46add420e880217c59d9c37
unknown <e.escher@J001-0291.etudiant.rubika-edu.com.local>
2017-10-11 13:56:34 +0200

    feature bowling operationnelle

--- Assets/Scripts/Balls/BallBowling.cs
+++ Assets/Scripts/Balls/BallBowling.cs
@@ -6,23 +6,25 @@ namespace VRBall
     {
         [Header("Bowling ball")]
         public GameObject quillPrefab;
-        public Transform[] marqueurPos;
+        private GameObject[] marqueurPos;
+        public Vector3 hauteur;
 
         private GameObject quillSpawned;
 
         protected new void Awake()
         {
             base.Awake();
+            marqueurPos = GameObject.FindGameObjectsWithTag("markers");
         }
 
         // Use this for initialization
         void Start()
         {
             // TODO fix Quill cannot spawned because prefab don't know the bowling markers.
-            // Vector3 pos = marqueurPos[Random.Range(0, marqueurPos.Length - 1)].position;
-            // pos.y += 3.0f;
+            Vector3 pos = marqueurPos[Random.Range(0, marqueurPos.Length - 1)].transform.position + hauteur;
+            
 
-            // quillSpawned = Instantiate(quillPrefab, pos, Quaternion.identity);
+            quillSpawned = Instantiate(quillPrefab, pos, Quaternion.identity);
         }
 
         protected new void Despawn()
