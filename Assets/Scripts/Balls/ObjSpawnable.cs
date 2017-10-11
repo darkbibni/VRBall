38cf8b238d9a8ea565d6c4bbe46ab8f445480d7d
Alexandre <alexndre.re@gmail.com>
2017-10-11 09:42:22 +0200

    Revert "Update Dest spawnobj"
    This reverts commit 42c264db6beb4b05d4c6c8182580937129cb3094.

---
+++ Assets/Scripts/Balls/ObjSpawnable.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace VRBall
+{
+    public class ObjSpawnable : MonoBehaviour
+    {
+        [Header("Object configuration")]
+        public float TimeEnable = 5;
+        
+        float saveTime;
+        bool OnHand = false;
+
+        // Couroutine booleans.
+        bool blinking = false;
+        bool dispawing = false;
+
+        protected void Awake()
+        {
+            saveTime = TimeEnable;
+        }
+
+        void Update()
+        {
+            // Don't doing anything if catched by player.
+            if (OnHand)
+                return;
+            
+            // Trigger Death when time is out.
+            if (TimeEnable <= 0f && !dispawing)
+            {
+                dispawing = true;
+
+                StopAllCoroutines();
+                Despawn();
+
+                return;
+            }
+
+            // Trigger blink effect when object will despawn.
+            else if (TimeEnable <= 5f && !blinking)
+            {
+                blinking = true;
+                StartCoroutine(Blink());
+            }
+            
+            TimeEnable -= Time.deltaTime;
+        }
+
+        private void UpdateTime() { 
+}
+
+        protected void Despawn()
+        {
+            GameManager.instance.LifePoints--;
+
+            StartCoroutine(FadeThenDestroy());
+        }
+
+        public void TakeOnHand(bool isOnHand)
+        {
+            if (isOnHand)
+            {
+                OnHand = true;
+                TimeEnable = saveTime;
+            }
+            else
+            {
+                OnHand = false;
+            }
+        }
+
+        private IEnumerator Blink()
+        {
+            // TODO FEEDBACK AUDIO AND GRAPHIC ?
+
+            yield return new WaitForSeconds(1.0f);
+        }
+
+        /// <summary>
+        /// Destroy item on a fade.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator FadeThenDestroy()
+        {
+            MeshRenderer renderer = GetComponent<MeshRenderer>();
+            Color c = renderer.material.color;
+
+            for (float alpha = 1.0f; alpha > 0.0f; alpha -= 0.1f)
+            {
+                c.a = alpha;
+                renderer.material.color = c;
+                yield return new WaitForSeconds(0.01f);
+            }
+            
+            Destroy(gameObject);
+        }
+
+        public void CatchObject()
+        {
+            OnHand = true;
+
+            // Reset timer ?
+            TimeEnable = saveTime;
+        }
+
+        public void ReleaseObject()
+        {
+            OnHand = false;
+        }
+    }
+}
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:Assets/Scripts/SpawnManager.cs
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -95,4 +96,110 @@ public class ObjectCaract
 	public GameObject ObjectPref;
 	public float PourcSpawn = 100;
 	public float MaxStartSpeed = 10;
+=======
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRBall {
+
+    public class SpawnManager : MonoBehaviour
+    {
+        public Vector2 TimeMinMaxSpawn;
+        public List<SpawnCaract> AllSpawn;
+
+        public int timeBeforeSpawn = 2;
+
+        int saveLast = 0;
+        int getCurr;
+
+        void Awake()
+        {
+            timeBeforeSpawn = (int)Random.Range(TimeMinMaxSpawn.x, TimeMinMaxSpawn.y);
+        }
+
+        void Update()
+        {
+            getCurr = (int)Time.timeSinceLevelLoad;
+
+            if (getCurr % timeBeforeSpawn == 0 && saveLast != getCurr)
+            {
+                timeBeforeSpawn = (int)Random.Range(TimeMinMaxSpawn.x, TimeMinMaxSpawn.y);
+                saveLast = getCurr;
+
+                newSpawn();
+            }
+        }
+
+        void newSpawn()
+        {
+            List<SpawnCaract> getAllSpawn = AllSpawn;
+            Transform getSpawnPos;
+            GameObject getNewObj;
+            Rigidbody getNewRig;
+            Vector3 getSize;
+            Vector3 newObjSize;
+
+            int a;
+            int b;
+            int c;
+            bool objSpawn;
+            int roomsUnlocked = GameManager.instance.RoomUnlocked;
+            
+            // Spawn balls in all spawners.
+            for (a = 0; a < roomsUnlocked; a++)
+            {
+                // Random number of balls spawned.
+                for (b = Random.Range(1, 3); b > 0; b--)
+                {
+                    objSpawn = false;
+                    
+                    // Spawn all balls.
+                    do
+                    {
+                        for (c = 0; c < getAllSpawn[a].ObjAttached.Count; c++)
+                        {
+                            if (Random.Range(0, 101) < getAllSpawn[a].ObjAttached[c].PourcSpawn)
+                            {
+                                objSpawn = true;
+
+                                getSpawnPos = getAllSpawn[a].SpawnPos;
+                                getNewObj = (GameObject)Instantiate(getAllSpawn[a].ObjAttached[c].ObjectPref, getSpawnPos);
+
+                                newObjSize = getNewObj.GetComponent<MeshRenderer>().bounds.size;
+                                newObjSize = new Vector3(newObjSize.x / 2, newObjSize.y / 2, newObjSize.z / 2);
+
+                                getSize = getSpawnPos.GetComponent<BoxCollider>().size;
+                                getSize = new Vector3(getSize.x / 2 - newObjSize.x, getSize.y / 2 - newObjSize.y, getSize.z / 2 - newObjSize.z);
+                                getSize = new Vector3(Random.Range(-getSize.x, getSize.x), Random.Range(-getSize.y, getSize.y), Random.Range(-getSize.z, getSize.z));
+
+                                getNewObj.transform.localPosition = getSize - getSpawnPos.up;
+
+                                getNewRig = getNewObj.GetComponent<Rigidbody>();
+                                getNewRig.velocity = -getSpawnPos.up * Random.Range(0, getAllSpawn[a].ObjAttached[c].MaxStartSpeed);
+                                
+                                break;
+                            }
+                        }
+                    } while (!objSpawn);
+                }
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class SpawnCaract
+    {
+        public Transform SpawnPos;
+        public List<ObjectCaract> ObjAttached;
+    }
+
+    [System.Serializable]
+    public class ObjectCaract
+    {
+        public GameObject ObjectPref;
+        public float PourcSpawn = 100;
+        public float MaxStartSpeed = 10;
+    }
+
+>>>>>>> parent of 42c264d... Update Dest spawnobj:Assets/Scripts/Managers/SpawnManager.cs
 }
