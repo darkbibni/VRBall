5551d40232cf056f758622073ae2e2ad84510385
Alexandre <Alexndre.re@gmail.com>
2017-10-11 13:24:29 +0200

    correction ui des balls

--- Assets/Scripts/Balls/LookPlayer.cs
+++ Assets/Scripts/Balls/LookPlayer.cs
@@ -6,16 +6,19 @@ public class LookPlayer : MonoBehaviour
 {
 	Transform currPlayer;
 	Transform thisBall;
+	Transform getParent;
 
-	void Awake ( )
+	void Start ( )
 	{
 		currPlayer = VRBall.GameManager.instance.GetPlayer;
 		thisBall = transform;
+		getParent = thisBall.parent;
 	}
 	
 	// Update is called once per frame
 	void Update ( ) 
 	{
+		thisBall.position = getParent.position + new Vector3 ( 0, 0.75f * getParent.localScale.y, 0 );
 		thisBall.LookAt ( currPlayer );
 	}
 }
